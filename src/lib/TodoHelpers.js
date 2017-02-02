0f878ae1f61aa1d3adb5c35240ea4dfa7e60e50a
Lazy Code <fbenalex@gmail.com>
2017-02-02 18:25:27 +0200

    Tests for helper functions

--- src/lib/TodoHelpers.js
+++ src/lib/TodoHelpers.js
@@ -1,3 +1,24 @@
 export const addTodo = (list, item) => [...list, item];
 
 export const generateId = () => Math.floor(Math.random()*100000);
+
+export const findById = (id, list) => list.find(item => item.id === id);
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete});
+
+// My way
+//export const updateTodo = (list, updated) => {
+//  return list.map(item => {
+//    return item.id !== updated.id ? item : updated;
+//  });
+//};
+
+//Tutorial way
+export const updateTodo = (list, updated) => {
+  const updatedIndex = list.findIndex(item => item.id === updated.id);
+  return [
+    ...list.slice(0, updatedIndex),
+    updated,
+    ...list.slice(updatedIndex+1)
+  ];
+};
